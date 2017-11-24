e9944a4e9d6c23d7e809e6b7b00cdbfc61b2ef3b
Eyassu <utopiaio@yahoo.com>
2017-11-24 09:23:32 +0300

    file:

--- app/redux/saga/user.js
+++ app/redux/saga/user.js
@@ -20,7 +20,7 @@ function* userBootFromLF() {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) { return; }
         js = d.createElement(s); js.id = id;
-        js.src = '//connect.facebook.net/en_US/sdk.js';
+        js.src = 'https://connect.facebook.net/en_US/sdk.js';
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));
     }
