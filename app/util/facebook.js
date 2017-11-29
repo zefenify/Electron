3687d6c6553824f01149eca2d3b7b374fdd4f6dc
Eyassu <utopiaio@yahoo.com>
2017-11-29 17:20:39 +0300

    `window.ELECTRON` existence checked before registering ipc events
    - development
    
    TODO:
    setup electron configuration

--- app/util/facebook.js
+++ app/util/facebook.js
@@ -8,43 +8,45 @@ import { FAUTH } from '@app/config/api';
 import { SONG_BOOT_REQUEST } from '@app/redux/constant/song';
 import { USER_REQUEST } from '@app/redux/constant/user';
 
-window.ELECTRON.ipcRenderer.on('facebookAccessToken', (e, accessToken) => {
-  api(`${FAUTH}${accessToken}`).then(({ data }) => {
-    store.dispatch({
-      type: USER_REQUEST,
-      payload: data,
-    });
+if (window.ELECTRON !== undefined) {
+  window.ELECTRON.ipcRenderer.on('facebookAccessToken', (e, accessToken) => {
+    api(`${FAUTH}${accessToken}`).then(({ data }) => {
+      store.dispatch({
+        type: USER_REQUEST,
+        payload: data,
+      });
 
-    // TODO: booters
-    // - [X] song
-    // - [ ] playlist
-    store.dispatch({
-      type: SONG_BOOT_REQUEST,
+      // TODO: booters
+      // - [X] song
+      // - [ ] playlist
+      store.dispatch({
+        type: SONG_BOOT_REQUEST,
+      });
+    }, () => {
+      store.dispatch({
+        type: NOTIFICATION_ON_REQUEST,
+        payload: {
+          message: 'ይቅርታ, Unable to reach Facebook server. Try again later',
+        },
+      });
     });
-  }, () => {
+  });
+
+  window.ELECTRON.ipcRenderer.on('facebookLoadError', () => {
     store.dispatch({
       type: NOTIFICATION_ON_REQUEST,
       payload: {
-        message: 'ይቅርታ, Unable to reach Facebook server. Try again later',
+        message: 'Unable to reach Facebook server',
       },
     });
   });
-});
 
-window.ELECTRON.ipcRenderer.on('facebookLoadError', () => {
-  store.dispatch({
-    type: NOTIFICATION_ON_REQUEST,
-    payload: {
-      message: 'Unable to reach Facebook server',
-    },
+  window.ELECTRON.ipcRenderer.on('facebookNoAccess', () => {
+    store.dispatch({
+      type: NOTIFICATION_ON_REQUEST,
+      payload: {
+        message: 'Access was not granted to Zefenify',
+      },
+    });
   });
-});
-
-window.ELECTRON.ipcRenderer.on('facebookNoAccess', () => {
-  store.dispatch({
-    type: NOTIFICATION_ON_REQUEST,
-    payload: {
-      message: 'Access was not granted to Zefenify',
-    },
-  });
-});
+}
--- app/util/mediaKeys.js
+++ app/util/mediaKeys.js
@@ -4,14 +4,16 @@
 import store from '@app/redux/store';
 import { NEXT_REQUEST, PREVIOUS_REQUEST, PLAY_PAUSE_REQUEST } from '@app/redux/constant/wolfCola';
 
-window.ELECTRON.ipcRenderer.on('PlayPause', () => {
-  store.dispatch({ type: PLAY_PAUSE_REQUEST });
-});
+if (window.ELECTRON !== undefined) {
+  window.ELECTRON.ipcRenderer.on('PlayPause', () => {
+    store.dispatch({ type: PLAY_PAUSE_REQUEST });
+  });
 
-window.ELECTRON.ipcRenderer.on('Next', () => {
-  store.dispatch({ type: NEXT_REQUEST });
-});
+  window.ELECTRON.ipcRenderer.on('Next', () => {
+    store.dispatch({ type: NEXT_REQUEST });
+  });
 
-window.ELECTRON.ipcRenderer.on('Previous', () => {
-  store.dispatch({ type: PREVIOUS_REQUEST });
-});
+  window.ELECTRON.ipcRenderer.on('Previous', () => {
+    store.dispatch({ type: PREVIOUS_REQUEST });
+  });
+}
